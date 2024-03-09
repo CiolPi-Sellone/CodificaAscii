f4009107112ff5e572eb366a761762b9326c6a25
Riccardo Bellavista <riccardo.bellavista@local.ittcesena.it>
2024-03-09 12:41:14 +0100

    PrimoUpload

---
+++ Codifiche_Ascii/Codifiche_Ascii/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+#region firma
+Console.WriteLine("Classe 3 \nAS 2023/24 ");
+//firma dell'autore
+#endregion
+
+//ASCII BASE
+char c; //inizializzazione variabile char
+Console.WriteLine("ASCII BASE");
+for (int i = 0; i < 128; i++)
+{
+    c = (char)i;
+    Console.WriteLine("Carattere numero " + i + ": " + c); //stampa a video dei caratteri
+}
+
+Console.WriteLine("Premi un tasto per chiudere il programma");
+Console.ReadKey();
